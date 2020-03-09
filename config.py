c0e07ded0866da171dbf77263abb15100df119cd
chanok203 <pathompatai_c@silpakorn.edu>
2020-03-09 22:03:30 +0700

    add MySQL

--- config.py
+++ config.py
@@ -6,5 +6,10 @@ load_dotenv(os.path.join(basedir, ".env"))
 
 
 class Config(object):
-    pass
 
+    SQLALCHEMY_DATABASE_URI = "mysql://%s:%s@%s/%s" % (
+        os.environ["MYSQL_USER"],
+        os.environ["MYSQL_PASSWORD"],
+        os.environ["MYSQL_HOST"],
+        os.environ["MYSQL_DATABASE"],
+    )
